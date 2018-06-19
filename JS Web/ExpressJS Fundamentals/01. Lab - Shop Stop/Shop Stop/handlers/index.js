ba0d4013b880fd54119b3402b5febfb321b169bf
T3ch0o <trottimusbg@gmail.com>
2018-06-19 21:37:31 +0300

    [updated]

--- JS Web/ExpressJS Fundamentals/01. Lab - Shop Stop/Shop Stop/handlers/index.js
+++
@@ -1,5 +0,0 @@
-const homeHandler = require('./home');
-const filesHandler = require('./static-files');
-const productHandler = require('./product');
-
-module.exports = [ homeHandler, filesHandler, productHandler ];
